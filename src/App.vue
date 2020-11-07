dd75defcb83fd587fc4362f454430e33c01da687
moritz.wachter <moritz.wachter@exozet.com>
2020-11-07 17:47:41 +0100

    add Calendar view and basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,74 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Calendar></Calendar>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Handlee|Montserrat:300,400');
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    color: #fff;
+    background: #222;
+    margin: 0;
+    width: 100%;
+    max-width: 100%;
+
+    font-size: 100%;
+  }
+
+  a {
+    color: white;
+    outline: none;
+  }
+
+  h1 {
+    font-size: 6rem;
+    text-shadow: 0 2px 8px #666;
+    margin: 50px;
+
+  @media screen and (max-width: 900px) {
+    font-size: 3rem;
+    margin: 30px;
+  }
+
+  @media screen and (max-width: 400px) {
+    font-size: 2rem;
+    margin: 25px;
+  }
+  }
+
+  h1, h2 {
+    font-family: 'Handlee', cursive;
+    text-align: center;
+  }
+
+  #video-background {
+    /*  making the video fullscreen  */
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+  }
 </style>
+
+<script>
+import Calendar from "@/views/Calendar";
+export default {
+  components: {Calendar}
+}
+</script>
---
+++ src/views/Calendar.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <h1>Merry Christmas</h1>
+    <h2>to you!</h2>
+    <div class="cards-wrapper">
+      <ul class="cards">
+        <li
+          class="card"
+          v-for="i in range(1,24)"
+          :key="i"
+        >
+          <div>
+            <router-link :to="'/day/' + i">
+              {{ i }}
+            </router-link>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  methods: {
+    range: function (min, max) {
+      let arr = []
+      let j = 0
+      for (let i = min; i <= max; i++) {
+        arr[j++] = i
+      }
+
+      return arr
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cards-wrapper {
+  width: 1200px;
+  height: 600px;
+  margin: 0 auto;
+  max-width: 100%;
+}
+
+.cards {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+
+  @media screen and (max-width: 385px) {
+    justify-content: center;
+  }
+}
+
+.card {
+  width: 100px;
+  height: 100px;
+
+  border-radius: 25px;
+
+  margin: 35px;
+
+  background: rgba(#000, 0.1);
+  border: 1px solid #fff;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: all 0.5s;
+
+  &:hover {
+    cursor: pointer;
+    background: rgba(#000, 0.3);
+    box-shadow: 0 0 20px rgba(#fff ,0.3);
+  }
+
+  a {
+    outline: 0;
+    color: #fff;
+    text-decoration: none;
+
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:active,
+    &:focus {
+      border: none;
+    }
+  }
+
+  div {
+    font-family: 'Handlee', cursive;
+    font-size: 52px;
+    font-weight: bold;
+    padding-top: 5px;
+
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
